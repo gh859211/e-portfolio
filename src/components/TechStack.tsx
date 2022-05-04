70714d07e666956481059a12d46c3b341c19dd43
cryptopal85 <gurkanhuray@gm-macmini.local>
2022-05-04 19:16:00 +0000

    added some additional icons and tech stacks

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,10 +1,15 @@
 import clsx from 'clsx';
 import * as React from 'react';
-import { IoLogoVercel } from 'react-icons/io5';
+import { GiRadarSweep } from 'react-icons/gi';
+import { IoLogoVercel, IoSkullOutline } from 'react-icons/io5';
 import {
+  SiBulma,
+  SiFlask,
+  SiHtml5,
   SiNextdotjs,
-  SiNodedotjs,
+  SiPython,
   SiReact,
+  SiRoamresearch,
   SiTailwindcss,
   SiTypescript,
 } from 'react-icons/si';
@@ -62,11 +67,47 @@ const stacks = [
         <CustomLink href='https://www.typescriptlang.org/'>
           TypeScript
         </CustomLink>
-        , finally jumping on this one, I love the experience! Check out my{' '}
-        <CustomLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-          starter template
-        </CustomLink>{' '}
-        using Next.js, Tailwind CSS, and TypeScript
+        , finally jumping on this one, I love the experience!
+      </>
+    ),
+  },
+  {
+    id: 'python',
+    icon: SiPython,
+    tooltip: (
+      <>
+        <CustomLink href='https://www.python.org'>Python</CustomLink> is
+        awesome, solid general-purpose programming language. Make sure to get
+        virtualenv and pyenv before downloading{' '}
+        <CustomLink href='https://www.python.org/downloads'>binary</CustomLink>.
+      </>
+    ),
+  },
+  {
+    id: 'flask',
+    icon: SiFlask,
+    tooltip: (
+      <>
+        <CustomLink href='https://flask.palletsprojects.com'>Flask</CustomLink>{' '}
+        is a micro web framework written in Python. It is classified as a
+        microframework because it does not require particular tools or libraries
+        - grab it{' '}
+        <CustomLink href='https://flask.palletsprojects.com/en/2.1.x/installation'>
+          here
+        </CustomLink>
+        .
+      </>
+    ),
+  },
+  {
+    id: 'html',
+    icon: SiHtml5,
+    tooltip: (
+      <>
+        <CustomLink href='https://whatwg.org'>HTML</CustomLink> is the standard
+        markup language for documents designed to be displayed in a web browser
+        - grab it{' '}
+        <CustomLink href='sorry you cannot'>oh no you cannot!</CustomLink>.
       </>
     ),
   },
@@ -78,8 +119,23 @@ const stacks = [
         <CustomLink href='https://tailwindcss.com/'>Tailwind CSS</CustomLink> is
         awesome, I have never achieved this much reusability. Make sure you get
         the{' '}
-        <CustomLink href='https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss'>
+        <CustomLink href='https://extensions.panic.com/extensions/jasonplatts/jasonplatts.tailwindcss/'>
           extension
+        </CustomLink>
+        .
+      </>
+    ),
+  },
+  {
+    id: 'bulma',
+    icon: SiBulma,
+    tooltip: (
+      <>
+        <CustomLink href='https://bulma.io'>Bulma</CustomLink> is a free, open
+        source framework that provides ready-to-use frontend components that you
+        can easily combine to build responsive web interfaces - grab it{' '}
+        <CustomLink href='https://bulma.io/documentation/overview/start'>
+          here
         </CustomLink>
         .
       </>
@@ -100,13 +156,39 @@ const stacks = [
     ),
   },
   {
-    id: 'node',
-    icon: SiNodedotjs,
+    id: 'nessus',
+    icon: GiRadarSweep,
     tooltip: (
       <>
-        <CustomLink href='https://nodejs.org/'>Node.js</CustomLink>, simple
-        backend language so you don't need to learn another language. Not using
-        this too often because Next.js already has a backend built-in.
+        <CustomLink href='https://www.tenable.com'>Nessus</CustomLink> is a
+        proprietary vulnerability scanner developed by Tenable, Inc. always
+        competes with Rapid7 - grab it{' '}
+        <CustomLink href='https://www.tenable.com/products/nessus/nessus-professional'>
+          here
+        </CustomLink>
+        .
+      </>
+    ),
+  },
+  {
+    id: 'research',
+    icon: SiRoamresearch,
+    tooltip: (
+      <>
+        <CustomLink href=''>Research</CustomLink> sorry you need to develop this
+        on your own! - grab it{' '}
+        <CustomLink href=''>oh no you cannot!</CustomLink>.
+      </>
+    ),
+  },
+  {
+    id: 'risk',
+    icon: IoSkullOutline,
+    tooltip: (
+      <>
+        <CustomLink href=''>Risk Management</CustomLink> sorry you need to
+        develop this on your own! - grab it{' '}
+        <CustomLink href=''>oh no you cannot!</CustomLink>.
       </>
     ),
   },
